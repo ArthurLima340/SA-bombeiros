2005b417da1416c0bf6e2c761f9ad686798dc8b3
ArthurLima340 <118366094+ArthurLima340@users.noreply.github.com>
2023-10-09 12:23:03 -0300

    atualização

---
+++ banco/conexao.php
@@ -0,0 +1,15 @@
+<?php
+//cofiguração o banco de dados 
+$host = 'localhost';
+$usuario = 'root';
+$senha = 'root';
+$banco = 'sa_bombeiros';
+//criação de conexão
+$conexao = new mysqli($host,$usuario,$senha,$banco);
+
+if($conexao->connect_error){
+
+    die("erro ns conexão com o banco de dados:" .$conexao->connect_error);
+}
+
+?>
