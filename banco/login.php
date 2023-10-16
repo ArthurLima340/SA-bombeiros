94afa9ad60afad0077601da573bced701ff822df
ArthurLima340 <118366094+ArthurLima340@users.noreply.github.com>
2023-10-16 11:04:42 -0300

    atualização

--- banco/login.php
+++ banco/login.php
@@ -4,23 +4,23 @@ include 'conexao.php';
 session_start();
 //varifica o formulário da tela login
 // if($_SERVER["REQUEST_METHOD"]==='POST'){
-    $email = $_POST['email'];
+    $nome_usuario = $_POST['nome_usuario'];
     $senha = $_POST['senha'];
 
-    $sql = "SELECT * FROM usuario WHERE email = ? AND senha = ?";
+    $sql = "SELECT * FROM login WHERE nome_usuario = ? AND senha = ?";
 
     $stmt = $conexao->prepare($sql);
-    $stmt->bind_param('ss', $email, $senha);
+    $stmt->bind_param('ss', $nome_usuario, $senha);
     $stmt->execute();
     $resultado = $stmt->get_result();
 
     if($resultado->num_rows === 1){
         //login foi efetuado com sucesso.
-        $_SESSION['email'] = $email;
+        $_SESSION['nome_usuario'] = $nome_usuario;
         header("Location: /login/pg-login.html");
         exit();
     }else{
-        echo "credenciais inválidas. Verifique seu email e senha";
+        echo "credenciais inválidas. Verifique seu nome_usuario e senha";
 }
     $conexao->close();
 ?>
