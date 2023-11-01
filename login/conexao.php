44924f4b25e8847eab138e77c50241f437bfc9e0
ArthurLima340 <118366094+ArthurLima340@users.noreply.github.com>
2023-11-01 11:59:13 -0300

    atualização

--- login/conexao.php
+++ login/conexao.php
@@ -1,15 +1,32 @@
 <?php
-//cofiguração o banco de dados 
-$host = 'localhost';
-$usuario = 'root';
-$senha = 'root';
-$banco = 'sa_bombeiros';
-//criação de conexão
-$conexao = new mysqli($host,$usuario,$senha,$banco);
+// Verifica se o formulário foi submetido
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Coleta os dados do formulário
+    $nome = $_POST["nome"];
 
-if($conexao->connect_error){
+    // Conexão com o banco de dados (substitua com suas próprias credenciais)
+    $hostname = "localhost";
+    $username = "root";
+    $password = "root";
+    $dbname = "sa_bombeiros";
 
-    die("erro ns conexão com o banco de dados:" .$conexao->connect_error);
+    $conn = new mysqli($hostname, $username, $password, $dbname);
+
+    // Verifica se a conexão foi estabelecida com sucesso
+    if ($conn->connect_error) {
+        die("Erro na conexão com o banco de dados: " . $conn->connect_error);
+    }
+
+    // // Consulta SQL para inserir os dados no banco de dados
+    // $sql = "INSERT INTO tabela (nome) VALUES ('$nome')";
+
+    // if ($conn->query($sql) === TRUE) {
+    //     echo "Dados inseridos com sucesso.";
+    // } else {
+    //     echo "Erro ao inserir os dados: " . $conn->error;
+    // }
+
+    // // Fecha a conexão com o banco de dados
+    // $conn->close();
 }
-
 ?>
