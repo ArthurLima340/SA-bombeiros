1db65b7a1d1563c6fa855792f43f84447f7e8164
geovanajochem <118366242+geovanajochem@users.noreply.github.com>
2023-11-23 20:58:22 -0300

    sa_bombeiros.sql

--- login/sa_bombeiros.sql
+++ login/sa_bombeiros.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 30-Out-2023 às 14:22
--- Versão do servidor: 8.0.21
--- versão do PHP: 8.1.2
+-- Tempo de geração: 24/11/2023 às 00:56
+-- Versão do servidor: 10.4.28-MariaDB
+-- Versão do PHP: 8.0.28
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,7 +24,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `anamnese_emergencia_medica`
+-- Estrutura para tabela `anamnese_emergencia_medica`
 --
 
 CREATE TABLE `anamnese_emergencia_medica` (
@@ -40,12 +40,12 @@ CREATE TABLE `anamnese_emergencia_medica` (
   `se_sim_especifique` varchar(100) NOT NULL,
   `ingeriu_alimento_ou_liquido>=6hrs` tinyint(1) NOT NULL,
   `que_hrs` time NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `anamnese_gestacional`
+-- Estrutura para tabela `anamnese_gestacional`
 --
 
 CREATE TABLE `anamnese_gestacional` (
@@ -65,12 +65,12 @@ CREATE TABLE `anamnese_gestacional` (
   `hr_nascimento` time NOT NULL,
   `sexo_bb` tinyint(1) NOT NULL,
   `nome_bb` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `avaliacao_cinematica`
+-- Estrutura para tabela `avaliacao_cinematica`
 --
 
 CREATE TABLE `avaliacao_cinematica` (
@@ -81,12 +81,12 @@ CREATE TABLE `avaliacao_cinematica` (
   `caminhando_na_cena` tinyint(1) NOT NULL,
   `painel_avariado` tinyint(1) NOT NULL,
   `volante_torcido` tinyint(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `dados_pessoais`
+-- Estrutura para tabela `dados_pessoais`
 --
 
 CREATE TABLE `dados_pessoais` (
@@ -100,12 +100,12 @@ CREATE TABLE `dados_pessoais` (
   `nome_acompanhante` varchar(50) NOT NULL,
   `idade_acompanhante` varchar(3) NOT NULL,
   `local_ocorrencia` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `decisao_transporte`
+-- Estrutura para tabela `decisao_transporte`
 --
 
 CREATE TABLE `decisao_transporte` (
@@ -113,51 +113,51 @@ CREATE TABLE `decisao_transporte` (
   `instavel` tinyint(1) NOT NULL,
   `potencialmente_instavel` tinyint(1) NOT NULL,
   `estavel` tinyint(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `divulgar_p_imprenca`
+-- Estrutura para tabela `divulgar_p_imprenca`
 --
 
 CREATE TABLE `divulgar_p_imprenca` (
   `sim` tinyint(1) NOT NULL,
   `nao` tinyint(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `forma_conducao`
+-- Estrutura para tabela `forma_conducao`
 --
 
 CREATE TABLE `forma_conducao` (
   `deitada` tinyint(1) NOT NULL,
   `sentada` tinyint(1) NOT NULL,
   `semi_sentada` tinyint(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `informacao_ocorrencia`
+-- Estrutura para tabela `informacao_ocorrencia`
 --
 
 CREATE TABLE `informacao_ocorrencia` (
-  `N_usb` int NOT NULL,
+  `N_usb` int(11) NOT NULL,
   `n_ocorrencia` varchar(10) NOT NULL,
   `desp` varchar(10) NOT NULL,
   `h_ch` varchar(10) NOT NULL,
   `km_final` varchar(10) NOT NULL,
-  `cod_ir` int NOT NULL,
-  `cod_ps` int NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `cod_ir` int(11) NOT NULL,
+  `cod_ps` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `localizacao_traumas`
+-- Estrutura para tabela `localizacao_traumas`
 --
 
 CREATE TABLE `localizacao_traumas` (
@@ -165,31 +165,12 @@ CREATE TABLE `localizacao_traumas` (
   `lado` varchar(20) NOT NULL,
   `face` varchar(20) NOT NULL,
   `tipo` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `login`
---
-
-CREATE TABLE `login` (
-  `nome_usuario` varchar(20) NOT NULL,
-  `senha` varchar(50) NOT NULL,
-  `registro_abvesc` varchar(6) NOT NULL,
-  `nome_completo_usuario` varchar(50) NOT NULL,
-  `funcao` varchar(50) NOT NULL,
-  `unidade` varchar(50) NOT NULL,
-  `n_unidade` varchar(3) NOT NULL,
-  `data_nasc` varchar(8) NOT NULL,
-  `cpf` varchar(11) NOT NULL,
-  `validade` varchar(8) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `materiais_utilizados_deixado_hospital`
+-- Estrutura para tabela `materiais_utilizados_deixado_hospital`
 --
 
 CREATE TABLE `materiais_utilizados_deixado_hospital` (
@@ -204,12 +185,12 @@ CREATE TABLE `materiais_utilizados_deixado_hospital` (
   `tirante_cabeca` varchar(2) NOT NULL,
   `canula` varchar(2) NOT NULL,
   `outro` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `materiais_utilizados_descartavel`
+-- Estrutura para tabela `materiais_utilizados_descartavel`
 --
 
 CREATE TABLE `materiais_utilizados_descartavel` (
@@ -225,16 +206,16 @@ CREATE TABLE `materiais_utilizados_descartavel` (
   `soro_fisiologico` varchar(2) NOT NULL,
   `talas_pap.` varchar(2) NOT NULL,
   `outro` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `problemas_encontrados`
+-- Estrutura para tabela `problemas_encontrados`
 --
 
 CREATE TABLE `problemas_encontrados` (
-  `id_problemas_encontrados` int NOT NULL,
+  `id_problemas_encontrados` int(11) NOT NULL,
   `psiquiatrico` tinyint(1) NOT NULL,
   `respiratório_DPOC` tinyint(1) NOT NULL,
   `respiratório_inalacao_fumaca` tinyint(1) NOT NULL,
@@ -244,12 +225,12 @@ CREATE TABLE `problemas_encontrados` (
   `obstetrico_gestante` tinyint(1) NOT NULL,
   `obstetrico_hemorragia_excessiva` tinyint(1) NOT NULL,
   `outro` tinyint(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `procedimentos_efetuados`
+-- Estrutura para tabela `procedimentos_efetuados`
 --
 
 CREATE TABLE `procedimentos_efetuados` (
@@ -288,7 +269,7 @@ CREATE TABLE `procedimentos_efetuados` (
   `tratado_choque` tinyint(1) NOT NULL,
   `uso_canula` tinyint(1) NOT NULL,
   `uso_colar` tinyint(1) NOT NULL,
-  `uso_colar_tamanho` int NOT NULL,
+  `uso_colar_tamanho` int(11) NOT NULL,
   `uso_ked` tinyint(1) NOT NULL,
   `uso_ttf` tinyint(1) NOT NULL,
   `ventilacao_suporte` tinyint(1) NOT NULL,
@@ -307,12 +288,12 @@ CREATE TABLE `procedimentos_efetuados` (
   `samu_usb` tinyint(1) NOT NULL,
   `cit` tinyint(1) NOT NULL,
   `outro` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `sinais_sintomas`
+-- Estrutura para tabela `sinais_sintomas`
 --
 
 CREATE TABLE `sinais_sintomas` (
@@ -368,16 +349,16 @@ CREATE TABLE `sinais_sintomas` (
   `taquicardia` tinyint(1) NOT NULL,
   `tontura` tinyint(1) NOT NULL,
   `outro` tinyint(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `tipo_ocorrencia`
+-- Estrutura para tabela `tipo_ocorrencia`
 --
 
 CREATE TABLE `tipo_ocorrencia` (
-  `id_tipo_ocorrencia` int NOT NULL,
+  `id_tipo_ocorrencia` int(11) NOT NULL,
   `causado_por_animais` tinyint(1) NOT NULL,
   `com_meio_transporte` tinyint(1) NOT NULL,
   `desmoronamento_deslizamento` tinyint(1) NOT NULL,
@@ -399,12 +380,12 @@ CREATE TABLE `tipo_ocorrencia` (
   `trabalho` tinyint(1) NOT NULL,
   `transferencia` tinyint(1) NOT NULL,
   `outro` varchar(10) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `transporte`
+-- Estrutura para tabela `transporte`
 --
 
 CREATE TABLE `transporte` (
@@ -415,12 +396,41 @@ CREATE TABLE `transporte` (
   `samu` tinyint(1) NOT NULL,
   `outro` tinyint(1) NOT NULL,
   `sem_remocao` tinyint(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `vitima_era`
+-- Estrutura para tabela `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `id` int(1) NOT NULL,
+  `nome_usuario` varchar(20) DEFAULT NULL,
+  `senha` varchar(15) DEFAULT NULL,
+  `cpf` varchar(11) DEFAULT NULL,
+  `nome_completo` varchar(30) DEFAULT NULL,
+  `funcao` varchar(19) DEFAULT NULL,
+  `registro_abvesc` int(11) DEFAULT NULL,
+  `n_unidade` int(11) DEFAULT NULL,
+  `unidade` varchar(20) DEFAULT NULL,
+  `data_nasc` varchar(10) DEFAULT NULL,
+  `validade` varchar(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Despejando dados para a tabela `usuario`
+--
+
+INSERT INTO `usuario` (`id`, `nome_usuario`, `senha`, `cpf`, `nome_completo`, `funcao`, `registro_abvesc`, `n_unidade`, `unidade`, `data_nasc`, `validade`) VALUES
+(1, 'arthur_lima', 'art123', '1076380015', 'Arthur de Lima Pereira', 'Bombeiro volunt?rio', 140001, 14, 'Joinville', '02/08/2005', '09/10/2024'),
+(2, 'user_adm', 'sou.adm', '', '', '', 0, 0, '', '', ''),
+(3, 'ana_ju', 'naju123', '13454347904', 'Ana Julia Briesemeister Xavier', 'Bombeiro volunt?rio', 160001, 16, 'Joinville', '16/06/2005', '09/10/2024');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `vitima_era`
 --
 
 CREATE TABLE `vitima_era` (
@@ -434,7 +444,27 @@ CREATE TABLE `vitima_era` (
   `trauma` tinyint(1) NOT NULL,
   `passageiro_moto` tinyint(1) NOT NULL,
   `pedestre` tinyint(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices de tabela `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `id` int(1) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
