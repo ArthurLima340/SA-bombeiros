5d3b9623234d65e0a5b19be9abd1f5a851392dea
geovanajochem <118366242+geovanajochem@users.noreply.github.com>
2023-11-27 22:40:42 -0300

    login.php

--- login/login.php
+++ login/login.php
@@ -1,27 +1,24 @@
 <?php
-include 'conexao.php';
+        include('conexao2.php'); /*arquivo de conexão com o bando de dados*/
 
-session_start();
-//varifica o formulário da tela login
-if($_SERVER["REQUEST_METHOD"]==='POST'){
-    $nome_usuario = $_POST['nome_usuario'];
-    $senha = $_POST['senha'];
-
-    $sql = "SELECT * FROM login WHERE nome_usuario = ? AND senha = ?";
-
-    $stmt = $conexao->prepare($sql);
-    $stmt->bind_param('ss', $nome_usuario, $senha);
-    $stmt->execute();
-    $resultado = $stmt->get_result();
-
-    if($resultado->num_rows === 1){
-        //login foi efetuado com sucesso.
-        $_SESSION['nome_usuario'] = $nome_usuario;
-        header("Location: pg-login.html");
-        exit();
-    }else{
-        echo "credenciais inválidas. Verifique seu nome_usuario e senha";
-}
-}
-    $conexao->close();
-?>
+        if(isset($_POST['button'])) {
+            $nome_usuario = $_POST['nome_usuario'];
+            $senha = $_POST['senha'];
+        
+            $sql = "SELECT * FROM usuario WHERE nome_usuario = ? AND senha = ?";
+           
+            $stmt = $conn->prepare($sql);
+            $stmt->bind_param('ss', $nome_usuario, $senha);
+            $stmt->execute();
+            $resultado = $stmt->get_result();
+        
+            if (mysqli_num_rows($resultado) === 1) {
+                header("Location: menu.html");
+            } else {
+                echo "Informações incorretas. Verifique seu usuário e senha";
+                /*echo "<script>document.querySelector('#form-text').innerText = 'E-mail ou senha inválidos'</script>";*/
+            }
+        }
+        
+        mysqli_close($conn);
+    ?>
